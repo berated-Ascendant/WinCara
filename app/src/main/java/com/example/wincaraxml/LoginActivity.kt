4f81087c446c112fa4489d290f5feeba3ca15a41
berated-Ascendant <beratedascendant@gmail.com>
2024-03-02 19:55:53 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/wincaraxml/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.wincaraxml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import android.content.Intent
+
+class LoginActivity : AppCompatActivity() {
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        installSplashScreen()
+        setContentView(R.layout.activity_login)
+
+
+
+       val signupBtn = findViewById<Button>(R.id.sign_up)
+        signupBtn.setOnClickListener(){
+            val intent = Intent(this,SignupActivity:: class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/wincaraxml/SignupActivity.kt
@@ -0,0 +1,14 @@
+package com.example.wincaraxml
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+
+}
